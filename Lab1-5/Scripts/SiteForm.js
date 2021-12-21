7f87e0031051229e063b6d439307ff8074f35d52
ParzivalP1 <64081744+ParzivalP1@users.noreply.github.com>
2021-12-22 00:53:41 +0300

    Add files via upload

---
+++ Lab1-5/Scripts/SiteForm.js
@@ -0,0 +1,50 @@
+var tab1es =
+    {
+        queue:
+            [
+                 // {id: 0, name:"In the middle of Hall"},
+                 // {id: 1, name:"Near the window"},
+                 // {id: 2, name:"three"},
+                 // {id: 3, name:"Next to the picture"},
+                 // {id: 4, name:"Next to the fireplace"}
+            ]
+    };
+
+outputIt();
+
+function outputIt() {
+    var restoredTab1es = JSON.parse(localStorage.getItem('tab1es'));
+    var outputs = "";
+    for(var i = 0; i < restoredTab1es.queue.length; i++)
+    {
+        outputs += '<div id="'+restoredTab1es.queue[i].id + '">' + restoredTab1es.queue[i].id+':'+restoredTab1es.queue[i].name + '</div>';
+    }
+    document.getElementById("demo").innerHTML= outputs;
+}
+function popIt() {
+    var restoredTab1es  = JSON.parse(localStorage.getItem('tab1es'));
+    restoredTab1es.queue.shift();
+    localStorage.setItem('tab1es', JSON.stringify(restoredTab1es));
+    outputIt();
+}
+debugger
+let i = 0
+function pushIt() {
+    if (JSON.parse(localStorage.getItem('tab1es')).queue.length === 5){
+        alert("Можно создать только 5 эементов, если хотите создать новые элементы, то сначала удалите старые");
+        //
+        // localStorage.getItem('tab1es').valueOf().endsWith(tab1es.queue.
+        return;
+
+    }
+    else {
+        var restoredTab1es = JSON.parse(localStorage.getItem('tab1es'));
+        i++
+        restoredTab1es.queue.push({
+            id:  i, name: $('input').val()});
+        localStorage.setItem('tab1es', JSON.stringify(restoredTab1es));
+        outputIt();
+        if (i === 5)
+            i = 0
+    }
+}
